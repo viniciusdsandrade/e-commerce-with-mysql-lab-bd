cc4893f8eec99163b6f919b90f5fd138879d077b
Andrade <vinicius_andrade2010@hotmail.com>
2024-08-27 14:22:33 -0300

    feat: db_social.sql

--- banco-de-dados/db_papelaria.sql
+++ banco-de-dados/db_papelaria.sql
@@ -1,25 +1,10 @@
-DROP DATABASE IF EXISTS db_papelaria;
-CREATE DATABASE IF NOT EXISTS db_papelaria;
-USE db_papelaria;
-
-CREATE TABLE IF NOT EXISTS tb_usuarios
-(
-    id_usuario           INT AUTO_INCREMENT PRIMARY KEY,
-    nome                 VARCHAR(100),
-    CPF                  VARCHAR(20),
-    telefone             VARCHAR(20),
-    email                VARCHAR(100),
-    senha                VARCHAR(100),
-    email_de_recuperacao VARCHAR(100) NULL,
-    id_endereco          INT,
-
-    UNIQUE (email)
-);
+DROP DATABASE IF EXISTS db_papelaria_livraria;
+CREATE DATABASE IF NOT EXISTS db_papelaria_livraria;
+USE db_papelaria_livraria;
 
 CREATE TABLE IF NOT EXISTS tb_enderecos
 (
-    id_endereco INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario  INT,
+    id_endereco BIGINT UNSIGNED AUTO_INCREMENT,
     endereco    VARCHAR(100),
     numero      VARCHAR(10),
     complemento VARCHAR(100) NULL,
@@ -28,109 +13,133 @@ CREATE TABLE IF NOT EXISTS tb_enderecos
     estado      VARCHAR(100),
     CEP         VARCHAR(20),
 
-    FOREIGN KEY (id_usuario) REFERENCES tb_usuarios (id_usuario)
+    PRIMARY KEY (id_endereco)
+);
+
+CREATE TABLE IF NOT EXISTS tb_usuarios
+(
+    id_usuario           BIGINT UNSIGNED AUTO_INCREMENT,
+    nome                 VARCHAR(100),
+    CPF                  VARCHAR(20),
+    telefone             VARCHAR(20),
+    email                VARCHAR(100),
+    senha                VARCHAR(100),
+    email_de_recuperacao VARCHAR(100)    NULL,
+    id_endereco          BIGINT UNSIGNED NULL, -- Pode ser NULL, pois o usuário pode não ter um endereço cadastrado
+
+    UNIQUE (CPF),
+    UNIQUE (email),
+
+    PRIMARY KEY (id_usuario),
+
+    FOREIGN KEY (id_endereco) REFERENCES tb_enderecos (id_endereco)
 );
 
 CREATE TABLE IF NOT EXISTS tb_produtos
 (
-    id_produto INT AUTO_INCREMENT PRIMARY KEY,
-    nome       VARCHAR(100),
-    preco      DECIMAL(10, 2),
-    descricao  TEXT
+    id_produto BIGINT UNSIGNED AUTO_INCREMENT,
+    nome       VARCHAR(100)   NOT NULL,
+    preco      DECIMAL(10, 2) NOT NULL,
+    descricao  TEXT           NULL, -- Pode ser NULL, pois o produto pode não ter descrição
 
+    PRIMARY KEY (id_produto)
 );
 
 CREATE TABLE IF NOT EXISTS tb_categorias
 (
-    id_categoria INT AUTO_INCREMENT,
-    nome         VARCHAR(100),
+    id_categoria BIGINT UNSIGNED AUTO_INCREMENT,
+    nome         VARCHAR(100) NOT NULL,
 
     PRIMARY KEY (id_categoria)
 );
 
 CREATE TABLE IF NOT EXISTS tb_produtos_categorias
 (
-    id_produto   INT,
-    id_categoria INT,
+    id_produto   BIGINT UNSIGNED NOT NULL,
+    id_categoria BIGINT UNSIGNED NOT NULL,
+
     FOREIGN KEY (id_produto) REFERENCES tb_produtos (id_produto),
     FOREIGN KEY (id_categoria) REFERENCES tb_categorias (id_categoria)
 );
 
-# Uma que associa o carrinho com todos os produtos selecionados
-
 CREATE TABLE IF NOT EXISTS tb_carrinho
 (
-    id_carrinho INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario  INT,
+    id_carrinho BIGINT UNSIGNED AUTO_INCREMENT,
+    id_usuario  BIGINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (id_carrinho),
+
     FOREIGN KEY (id_usuario) REFERENCES tb_usuarios (id_usuario)
 );
 
 CREATE TABLE IF NOT EXISTS tb_carrinho_produtos
 (
-    id_carrinho INT,
-    id_produto  INT,
-    quantidade  INT,
+    id_carrinho BIGINT UNSIGNED NOT NULL,
+    id_produto  BIGINT UNSIGNED NOT NULL,
+    quantidade  BIGINT UNSIGNED NOT NULL,
+
     FOREIGN KEY (id_carrinho) REFERENCES tb_carrinho (id_carrinho),
     FOREIGN KEY (id_produto) REFERENCES tb_produtos (id_produto)
 );
 
-
-# Tabela de log das compras realizadas
-
 CREATE TABLE IF NOT EXISTS tb_compras
 (
-    id_compra         INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario        INT,
-    data_compra       DATETIME,
-    id_transportadora INT,
+    id_compra         BIGINT UNSIGNED AUTO_INCREMENT,
+    id_usuario        BIGINT UNSIGNED NOT NULL,
+    data_compra       DATETIME        NOT NULL,
+    id_transportadora BIGINT UNSIGNED NULL, -- Pode ser NULL, pois a compra pode não ter uma transportadora
+
+    PRIMARY KEY (id_compra),
 
     FOREIGN KEY (id_usuario) REFERENCES tb_usuarios (id_usuario)
 );
 
-# Cada compra pode 0 ou n promoções/cupons
-# e cada promoção/cupom pode estar associado a 1 ou n produtos(da compra) --> tb_compras_produtos
+CREATE TABLE IF NOT EXISTS tb_promocoes
+(
+    id_promocao BIGINT UNSIGNED AUTO_INCREMENT,
+    nome        VARCHAR(100)   NULL,
+    desconto    DECIMAL(10, 2) NOT NULL,
+    data_inicio DATETIME       NOT NULL,
+    data_fim    DATETIME       NOT NULL,
+
+    PRIMARY KEY (id_promocao)
+);
 
 CREATE TABLE IF NOT EXISTS tb_compras_promocoes
 (
-    id_compra   INT,
-    id_promocao INT,
+    id_compra   BIGINT UNSIGNED NOT NULL,
+    id_promocao BIGINT UNSIGNED NOT NULL,
 
     FOREIGN KEY (id_compra) REFERENCES tb_compras (id_compra),
     FOREIGN KEY (id_promocao) REFERENCES tb_promocoes (id_promocao)
 );
 
-CREATE TABLE IF NOT EXISTS tb_promocoes
+
+CREATE TABLE IF NOT EXISTS tb_cupons
 (
-    id_promocao INT AUTO_INCREMENT PRIMARY KEY,
-    nome        VARCHAR(100) NULL,
-    desconto    DECIMAL(10, 2),
-    data_inicio DATETIME,
-    data_fim    DATETIME
+    id_cupom    BIGINT UNSIGNED AUTO_INCREMENT,
+    nome        VARCHAR(100)   NULL,
+    desconto    DECIMAL(10, 2) NOT NULL,
+    data_inicio DATETIME       NOT NULL,
+    data_fim    DATETIME       NOT NULL,
+
+    PRIMARY KEY (id_cupom)
 );
 
 CREATE TABLE IF NOT EXISTS tb_compras_cupons
 (
-    id_compra INT,
-    id_cupom  INT,
+    id_compra BIGINT UNSIGNED NOT NULL,
+    id_cupom  BIGINT UNSIGNED NOT NULL,
 
     FOREIGN KEY (id_compra) REFERENCES tb_compras (id_compra),
     FOREIGN KEY (id_cupom) REFERENCES tb_cupons (id_cupom)
 );
 
-CREATE TABLE IF NOT EXISTS tb_cupons
-(
-    id_cupom    INT AUTO_INCREMENT PRIMARY KEY,
-    nome        VARCHAR(100),
-    desconto    DECIMAL(10, 2),
-    data_inicio DATETIME,
-    data_fim    DATETIME
-);
-
 CREATE TABLE IF NOT EXISTS tb_compras_produtos
 (
-    id_compra  INT,
-    id_produto INT,
-    quantidade INT,
+    id_compra  BIGINT UNSIGNED NOT NULL,
+    id_produto BIGINT UNSIGNED NOT NULL,
+    quantidade BIGINT UNSIGNED NOT NULL,
 
     FOREIGN KEY (id_compra) REFERENCES tb_compras (id_compra),
     FOREIGN KEY (id_produto) REFERENCES tb_produtos (id_produto)
@@ -138,18 +147,18 @@ CREATE TABLE IF NOT EXISTS tb_compras_produtos
 
 CREATE TABLE IF NOT EXISTS tb_transportadoras
 (
-    id_transportadora  INT AUTO_INCREMENT,
-    nome               VARCHAR(100),
-    preco_por_km       DECIMAL(10, 2),
-    CNPJ               VARCHAR(20),
-    inscricao_estadual VARCHAR(100),
-    telefone           VARCHAR(20),
-    email              VARCHAR(100),
-    endereco           VARCHAR(100),
+    id_transportadora  BIGINT UNSIGNED AUTO_INCREMENT,
+    nome               VARCHAR(100)   NOT NULL,
+    preco_por_km       DECIMAL(10, 2) NULL,
+    CNPJ               VARCHAR(20)    NOT NULL,
+    inscricao_estadual VARCHAR(100)   NOT NULL,
+    telefone           VARCHAR(20)    NOT NULL,
+    email              VARCHAR(100)   NOT NULL,
+    endereco           VARCHAR(100)   NULL,
+
+    UNIQUE (CNPJ),
+    UNIQUE (inscricao_estadual),
+    UNIQUE (email),
 
     PRIMARY KEY (id_transportadora)
 );
-
-
-
-
---
+++ banco-de-dados/db_social.sql
@@ -0,0 +1,104 @@
+DROP DATABASE IF EXISTS db_social;
+CREATE DATABASE IF NOT EXISTS db_social;
+USE db_social;
+
+/*
+Imagine que você está desenvolvendo um aplicatico de rede social onde os usuários podem postar atualizações e comnetários que podem incluir emojis
+
+ nestes casos, você precisará de suporte parra o conjunto comple de caracteres unicode,
+ incluindo emojis, o que requer o uso de UTF8MB4
+
+ crie as tabelas e o relacionamento enetre elas
+ */
+
+CREATE TABLE users
+(
+    id         BIGINT UNSIGNED AUTO_INCREMENT,
+    username   VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (username),
+    UNIQUE (email),
+
+    PRIMARY KEY (id)
+) CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE posts
+(
+    id         BIGINT UNSIGNED AUTO_INCREMENT,
+    user_id    BIGINT UNSIGNED NOT NULL,
+    content    TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (user_id) REFERENCES users (id)
+) CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE comments
+(
+    id         BIGINT UNSIGNED AUTO_INCREMENT,
+    post_id    BIGINT UNSIGNED NOT NULL,
+    user_id    BIGINT UNSIGNED NOT NULL,
+    content    TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (post_id) REFERENCES posts (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+) CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE likes
+(
+    id         BIGINT UNSIGNED AUTO_INCREMENT,
+    user_id    BIGINT UNSIGNED NOT NULL,
+    post_id    BIGINT UNSIGNED NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+
+
+-- Inserir usuários
+INSERT INTO users (username, email, password)
+VALUES ('joao_silva', 'joao@example.com', 'senha123'),
+       ('maria_costa', 'maria@example.com', 'segredo456'),
+       ('pedro_henrique', 'pedro@example.com', 'p@$$wOrd');
+
+-- Inserir posts
+INSERT INTO posts (user_id, content)
+VALUES (1, 'Olá, este é meu primeiro post! 😁'),
+       (2, 'Adorei essa nova rede social! 😊'),
+       (1, 'Alguém aí gosta de programar? 🤔');
+
+-- Inserir comentários
+INSERT INTO comments (post_id, user_id, content)
+VALUES (1, 2, 'Que legal, João! 👏'),
+       (2, 3, 'Eu também! 🤩'),
+       (3, 1, 'Sim, eu adoro! 😄');
+
+-- Inserir likes
+INSERT INTO likes (user_id, post_id)
+VALUES (1, 2),
+       (2, 1),
+       (3, 3),
+       (2, 3);
+
+
+SELECT *
+FROM users;
+SELECT *
+FROM posts;
+SELECT *
+FROM comments;
+SELECT *
+FROM likes;
