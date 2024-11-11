13c973e13b111e8c714d562fd6c0c385bcde7b34
Felipe <felipe.arnemom@gmail.com>
2024-11-11 20:07:07 -0300

    Página de categorias funcionando

--- livraria_papelaria/app/views.py
+++ livraria_papelaria/app/views.py
@@ -1,4 +1,37 @@
 from django.shortcuts import render
 
-def teste(request):
+def index(request):
     return render(request, 'index.html')
+
+def categorias(request):
+    context = {
+        'categorias': [ # Aqui tem que fazer uma query no banco de dados
+            'Aventura',
+            'Romance',
+            'Terror',
+            'Ficção Científica',
+            'Fantasia',
+            'Suspense',
+            'Drama',
+            'Biografia',
+            'História',
+            'Infantil',
+            'Autoajuda',
+            'Religião',
+            'Quadrinhos',
+            'Mangá',
+            'Culinária',
+            'Arte',
+            'Esporte',
+            'Música',
+            'Cinema',
+            'Moda',
+            'Arquitetura',
+            'Design',
+            'Ciência',
+            'Tecnologia',
+            'Negócios',
+        ]
+    }
+
+    return render(request, 'categorias.html', context)
