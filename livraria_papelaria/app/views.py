4edda4d1a9f281df70de3bb2f05de73c24002f1f
Felipe de Carvalho Santos <142360153+Felipinho5@users.noreply.github.com>
2024-11-14 09:11:29 -0300

    .

--- livraria_papelaria/app/views.py
+++ livraria_papelaria/app/views.py
@@ -11,34 +11,4 @@ def pesquisa(request):
 
 
 def categorias(request):
-    context = {
-        'categorias': [ # Aqui tem que fazer uma query no banco de dados
-            'Aventura',
-            'Romance',
-            'Terror',
-            'Ficção Científica',
-            'Fantasia',
-            'Suspense',
-            'Drama',
-            'Biografia',
-            'História',
-            'Infantil',
-            'Autoajuda',
-            'Religião',
-            'Quadrinhos',
-            'Mangá',
-            'Culinária',
-            'Arte',
-            'Esporte',
-            'Música',
-            'Cinema',
-            'Moda',
-            'Arquitetura',
-            'Design',
-            'Ciência',
-            'Tecnologia',
-            'Negócios',
-        ]
-    }
-
-    return render(request, 'categorias.html', context)
+    return render(request, 'categorias.html')
