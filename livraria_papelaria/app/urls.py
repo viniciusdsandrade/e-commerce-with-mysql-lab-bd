9a1ab3d456767f57b7463984f4da7e339e48f4a5
Felipe <felipe.arnemom@gmail.com>
2024-11-14 21:49:13 -0300

    Página de pesquisa e info de produto prontas

--- livraria_papelaria/app/urls.py
+++ livraria_papelaria/app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('categorias/', views.categorias, name='categorias'),
     path('pesquisa/', views.pesquisa, name='pesquisa'),
+    path('produto/', views.produto, name='produto'),
 ]
--- livraria_papelaria/app/views.py
+++ livraria_papelaria/app/views.py
@@ -4,11 +4,13 @@ def index(request):
     return render(request, 'index.html')
 
 
-
 def pesquisa(request):
     return render(request, 'pesquisa.html')
 
 
-
 def categorias(request):
     return render(request, 'categorias.html')
+
+
+def produto(request):
+    return render(request, 'produto.html')
