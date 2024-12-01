559c4a6e26fa9324429ab3bd78159c4280ba64e8
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2024-12-01 00:02:54 -0300

    doc:

--- banco-de-dados/livraria-papelaria/mysql/db_papelaria_livraria.sql
+++ banco-de-dados/livraria-papelaria/mysql/db_papelaria_livraria.sql
@@ -1,8 +1,13 @@
+-- Remove o banco de dados existente, se houver
 DROP DATABASE IF EXISTS db_papelaria_livraria;
+
+-- Cria o banco de dados caso não exista
 CREATE DATABASE IF NOT EXISTS db_papelaria_livraria;
+
+-- Seleciona o banco de dados para uso
 USE db_papelaria_livraria;
 
--- Tabela de Papéis (Roles)
+-- Tabela de Acessos (Roles) com permissões específicas
 CREATE TABLE IF NOT EXISTS tb_acesso
 (
     id                          BIGINT UNSIGNED AUTO_INCREMENT,
@@ -35,22 +40,21 @@ CREATE TABLE IF NOT EXISTS tb_acesso
     pode_editar_cupons          BOOLEAN DEFAULT FALSE,
     pode_remover_cupons         BOOLEAN DEFAULT FALSE,
 
-
     UNIQUE (nome),                                     -- Cada papel tem um nome único
     PRIMARY KEY (id)
 );
 
--- Tabela base para armazenar os campos comuns entre Funcionários e Usuários
+-- Tabela base para armazenar informações comuns de Pessoas (Funcionários e Usuários)
 CREATE TABLE IF NOT EXISTS tb_pessoa
 (
     id                BIGINT UNSIGNED AUTO_INCREMENT,
     nome              VARCHAR(100) NOT NULL, -- Nome da pessoa
-    data_nascimento   DATE         NULL,     -- Data de nascimento comum
-    cpf               VARCHAR(20)  NOT NULL, -- CPF único para ambas as tabelas
-    email             VARCHAR(100) NOT NULL, -- E-mail comum
-    senha             VARCHAR(255) NOT NULL, -- Senha comum
-    telefone          VARCHAR(20)  NULL,     -- Telefone comum
-    email_recuperacao VARCHAR(100) NULL,     -- E-mail de recuperação comum
+    data_nascimento   DATE         NULL,     -- Data de nascimento
+    cpf               VARCHAR(20)  NOT NULL, -- CPF único
+    email             VARCHAR(100) NOT NULL, -- E-mail único
+    senha             VARCHAR(255) NOT NULL, -- Senha
+    telefone          VARCHAR(20)  NULL,     -- Telefone
+    email_recuperacao VARCHAR(100) NULL,     -- E-mail de recuperação
 
     UNIQUE (cpf),                            -- CPF deve ser único
     UNIQUE (email),                          -- E-mail deve ser único
@@ -58,38 +62,35 @@ CREATE TABLE IF NOT EXISTS tb_pessoa
     PRIMARY KEY (id)
 );
 
--- Tabela para armazenar os dados específicos de Funcionários, referenciando tb_pessoa
+-- Tabela para armazenar dados específicos de Funcionários
 CREATE TABLE IF NOT EXISTS tb_funcionario
 (
     id_pessoa BIGINT UNSIGNED NOT NULL,                -- FK para tb_pessoa
-    id_acesso BIGINT UNSIGNED NOT NULL,                -- Nível de acesso para funcionários
+    id_acesso BIGINT UNSIGNED NOT NULL,                -- Nível de acesso
 
-
-    PRIMARY KEY (id_pessoa),                           -- Usa a PK da tabela pessoa como PK
-    FOREIGN KEY (id_pessoa) REFERENCES tb_pessoa (id), -- FK para a tabela pessoa
-    FOREIGN KEY (id_acesso) REFERENCES tb_acesso (id)  -- Nível de acesso referenciado na tb_role
+    PRIMARY KEY (id_pessoa),                           -- PK baseada em tb_pessoa
+    FOREIGN KEY (id_pessoa) REFERENCES tb_pessoa (id), -- FK para Pessoa
+    FOREIGN KEY (id_acesso) REFERENCES tb_acesso (id)  -- FK para Acesso
 );
 
--- Tabela para armazenar os dados específicos de Usuários, referenciando tb_pessoa
+-- Tabela para armazenar dados específicos de Usuários
 CREATE TABLE IF NOT EXISTS tb_usuario
 (
     id_pessoa            BIGINT UNSIGNED NOT NULL,                 -- FK para tb_pessoa
-    saldo                DECIMAL(10, 2)           DEFAULT 0,       -- Saldo associado ao usuário
+    saldo                DECIMAL(10, 2)           DEFAULT 0,       -- Saldo do usuário
     estado               ENUM (
         'AGUARDANDO_VERIFICACAO',
         'ATIVO',
         'SUSPENSO',
         'BANIDO'
         )                                NOT NULL DEFAULT 'ATIVO', -- Estado do usuário
-    data_final_suspensao DATETIME        NULL,                     -- Data de término de suspensão, se houver
+    data_final_suspensao DATETIME        NULL,                     -- Fim da suspensão
 
-
-    PRIMARY KEY (id_pessoa),                                       -- Usa a PK da tabela pessoa como PK
-    FOREIGN KEY (id_pessoa) REFERENCES tb_pessoa (id)              -- FK para a tabela pessoa
+    PRIMARY KEY (id_pessoa),                                       -- PK baseada em tb_pessoa
+    FOREIGN KEY (id_pessoa) REFERENCES tb_pessoa (id)              -- FK para Pessoa
 );
 
-
--- Tabela para armazenar as Transportadoras
+-- Tabela para armazenar Transportadoras
 CREATE TABLE IF NOT EXISTS tb_transportadora
 (
     id                 BIGINT UNSIGNED AUTO_INCREMENT,
@@ -108,12 +109,12 @@ CREATE TABLE IF NOT EXISTS tb_transportadora
     PRIMARY KEY (id)
 );
 
--- Atualizando a tabela de funcionários (sem necessidade de duplicação de dados)
+-- Tabela para armazenar Chamados de Suporte
 CREATE TABLE IF NOT EXISTS tb_chamado
 (
     id               BIGINT UNSIGNED AUTO_INCREMENT,
-    id_usuario       BIGINT UNSIGNED                                           NOT NULL, -- FK para a tabela de usuários
-    id_funcionario   BIGINT UNSIGNED                                           NOT NULL, -- FK para a tabela de funcionários
+    id_usuario       BIGINT UNSIGNED                                           NOT NULL, -- FK para Usuário
+    id_funcionario   BIGINT UNSIGNED                                           NOT NULL, -- FK para Funcionário
     titulo           VARCHAR(50)                                               NOT NULL,
     descricao        TEXT                                                      NOT NULL,
     estado           ENUM ('ABERTO', 'EM_ANDAMENTO', 'RESOLVIDO', 'CANCELADO') NOT NULL DEFAULT 'ABERTO',
@@ -121,25 +122,25 @@ CREATE TABLE IF NOT EXISTS tb_chamado
     data_finalizacao DATETIME                                                  NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_pessoa),                          -- Relaciona o usuário
-    FOREIGN KEY (id_funcionario) REFERENCES tb_funcionario (id_pessoa)                   -- Relaciona o funcionário
+    FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_pessoa),                          -- Relaciona ao Usuário
+    FOREIGN KEY (id_funcionario) REFERENCES tb_funcionario (id_pessoa)                   -- Relaciona ao Funcionário
 );
 
--- Tabela para registrar mensagens em chamados
+-- Tabela para registrar Mensagens em Chamados
 CREATE TABLE IF NOT EXISTS tb_mensagem
 (
     id           BIGINT UNSIGNED AUTO_INCREMENT,
-    id_chamado   BIGINT UNSIGNED NOT NULL,               -- FK para a tabela de chamado
-    id_remetente BIGINT UNSIGNED NOT NULL,               -- FK para a tabela de pessoa
-    texto        TEXT            NOT NULL,               -- A mensagem trocada
-    data_envio   DATETIME        NOT NULL,               -- Data e hora em que a mensagem foi enviada
+    id_chamado   BIGINT UNSIGNED NOT NULL,               -- FK para Chamado
+    id_remetente BIGINT UNSIGNED NOT NULL,               -- FK para Pessoa
+    texto        TEXT            NOT NULL,               -- Conteúdo da mensagem
+    data_envio   DATETIME        NOT NULL,               -- Data e hora do envio
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_chamado) REFERENCES tb_chamado (id), -- Relaciona à tabela de chamado
-    FOREIGN KEY (id_remetente) REFERENCES tb_pessoa (id) -- Relaciona à tabela de pessoa
+    FOREIGN KEY (id_chamado) REFERENCES tb_chamado (id), -- Relaciona ao Chamado
+    FOREIGN KEY (id_remetente) REFERENCES tb_pessoa (id) -- Relaciona ao Remetente
 );
 
--- Tabela para armazenar formas de pagamento
+-- Tabela para armazenar Formas de Pagamento dos Usuários
 CREATE TABLE IF NOT EXISTS tb_forma_pagamento
 (
     id         BIGINT UNSIGNED AUTO_INCREMENT,
@@ -149,7 +150,7 @@ CREATE TABLE IF NOT EXISTS tb_forma_pagamento
     FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_pessoa)
 );
 
--- Tabela para armazenar detalhes específicos do PIX
+-- Tabela para armazenar detalhes do PIX
 CREATE TABLE IF NOT EXISTS tb_pix
 (
     id_forma_pagamento BIGINT UNSIGNED NOT NULL,
@@ -159,7 +160,7 @@ CREATE TABLE IF NOT EXISTS tb_pix
     FOREIGN KEY (id_forma_pagamento) REFERENCES tb_forma_pagamento (id)
 );
 
--- Tabela para armazenar detalhes específicos do Cartão de Crédito
+-- Tabela para armazenar detalhes de Cartão de Crédito
 CREATE TABLE IF NOT EXISTS tb_cartao
 (
     id_forma_pagamento BIGINT UNSIGNED            NOT NULL,
@@ -173,7 +174,7 @@ CREATE TABLE IF NOT EXISTS tb_cartao
     FOREIGN KEY (id_forma_pagamento) REFERENCES tb_forma_pagamento (id)
 );
 
--- Tabela para armazenar endereços dos usuários
+-- Tabela para armazenar Endereços dos Usuários
 CREATE TABLE IF NOT EXISTS tb_endereco
 (
     id           BIGINT UNSIGNED AUTO_INCREMENT,
@@ -193,7 +194,7 @@ CREATE TABLE IF NOT EXISTS tb_endereco
     FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_pessoa)
 );
 
--- Tabela para armazenar os Produtos
+-- Tabela para armazenar Produtos
 CREATE TABLE IF NOT EXISTS tb_produto
 (
     id        BIGINT UNSIGNED AUTO_INCREMENT,
@@ -205,7 +206,7 @@ CREATE TABLE IF NOT EXISTS tb_produto
     PRIMARY KEY (id)
 );
 
--- Tabela para armazenar o Estoque dos Produtos
+-- Tabela para armazenar Estoque dos Produtos
 CREATE TABLE IF NOT EXISTS tb_estoque
 (
     id          BIGINT UNSIGNED AUTO_INCREMENT,
@@ -217,7 +218,7 @@ CREATE TABLE IF NOT EXISTS tb_estoque
     FOREIGN KEY (id_produto) REFERENCES tb_produto (id)
 );
 
--- Tabela para armazenar as Categorias
+-- Tabela para armazenar Categorias de Produtos
 CREATE TABLE IF NOT EXISTS tb_categoria
 (
     id   BIGINT UNSIGNED AUTO_INCREMENT,
@@ -226,7 +227,7 @@ CREATE TABLE IF NOT EXISTS tb_categoria
     PRIMARY KEY (id)
 );
 
--- Tabela de associação N:N entre Produtos e Categorias
+-- Tabela de Associação N:N entre Produtos e Categorias
 CREATE TABLE IF NOT EXISTS tb_produto_categoria
 (
     id_produto   BIGINT UNSIGNED NOT NULL,
@@ -236,7 +237,7 @@ CREATE TABLE IF NOT EXISTS tb_produto_categoria
     FOREIGN KEY (id_categoria) REFERENCES tb_categoria (id)
 );
 
--- Tabela para armazenar as Avaliações dos Produtos
+-- Tabela para armazenar Listas de Desejos dos Usuários
 CREATE TABLE IF NOT EXISTS tb_lista_desejos
 (
     id_usuario BIGINT UNSIGNED NOT NULL,
@@ -245,7 +246,7 @@ CREATE TABLE IF NOT EXISTS tb_lista_desejos
     FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_pessoa)
 );
 
--- Tabela de associação N:N entre Listas de Desejos e Produtos
+-- Tabela de Associação N:N entre Listas de Desejos e Produtos
 CREATE TABLE IF NOT EXISTS tb_lista_desejos_produto
 (
     id_produto       BIGINT UNSIGNED NOT NULL,
@@ -255,7 +256,7 @@ CREATE TABLE IF NOT EXISTS tb_lista_desejos_produto
     FOREIGN KEY (id_lista_desejos) REFERENCES tb_lista_desejos (id_usuario)
 );
 
--- Tabela para armazenar os Carrinhos de Compras de cada Usuário
+-- Tabela para armazenar Carrinhos de Compras dos Usuários
 CREATE TABLE IF NOT EXISTS tb_carrinho
 (
     id_usuario BIGINT UNSIGNED NOT NULL,
@@ -265,7 +266,7 @@ CREATE TABLE IF NOT EXISTS tb_carrinho
     FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id_pessoa)
 );
 
--- Tabela de associação N:N entre Carrinhos e Produtos
+-- Tabela de Associação N:N entre Carrinhos e Produtos
 CREATE TABLE IF NOT EXISTS tb_carrinho_produto
 (
     id_produto  BIGINT UNSIGNED NOT NULL,
@@ -276,7 +277,7 @@ CREATE TABLE IF NOT EXISTS tb_carrinho_produto
     FOREIGN KEY (id_carrinho) REFERENCES tb_carrinho (id_usuario)
 );
 
--- Tabela para armazenar as Compras
+-- Tabela para armazenar Compras realizadas pelos Usuários
 CREATE TABLE IF NOT EXISTS tb_compra
 (
     id             BIGINT UNSIGNED AUTO_INCREMENT,
@@ -290,7 +291,7 @@ CREATE TABLE IF NOT EXISTS tb_compra
     FOREIGN KEY (id_endereco) REFERENCES tb_endereco (id)
 );
 
--- Tabela para armazenar os produtos comprados em cada compra, além de suas opcionais avaliações
+-- Tabela para armazenar Produtos comprados em cada Compra
 CREATE TABLE IF NOT EXISTS tb_compra_produto
 (
     id         BIGINT UNSIGNED AUTO_INCREMENT,
@@ -306,7 +307,7 @@ CREATE TABLE IF NOT EXISTS tb_compra_produto
 ) CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
 
--- Tabela para armazenar as avaliações dos produtos comprados
+-- Tabela para armazenar Avaliações dos Produtos Comprados
 CREATE TABLE IF NOT EXISTS tb_avaliacao
 (
     id                BIGINT UNSIGNED AUTO_INCREMENT,
@@ -319,14 +320,14 @@ CREATE TABLE IF NOT EXISTS tb_avaliacao
         '⭐⭐⭐⭐',
         '⭐⭐⭐⭐⭐'
         ) CHARACTER SET utf8mb4
-                          COLLATE utf8mb4_unicode_ci NULL, -- Avaliação com emojis de estrelas (opcional)
-    comentario        TEXT                           NULL, -- Comentário (opcional),
+                          COLLATE utf8mb4_unicode_ci NULL, -- Avaliação com emojis de estrelas
+    comentario        TEXT                           NULL, -- Comentário opcional
 
     PRIMARY KEY (id),
     FOREIGN KEY (id_compra_produto) REFERENCES tb_compra_produto (id)
 );
 
--- Tabela de rastreamento de compras, com relação 1:1 com tb_compras
+-- Tabela de Rastreamento de Entregas das Compras
 CREATE TABLE IF NOT EXISTS tb_entrega
 (
     id_compra_produto       BIGINT UNSIGNED NOT NULL,
@@ -355,7 +356,7 @@ CREATE TABLE IF NOT EXISTS tb_entrega
     FOREIGN KEY (id_transportadora) REFERENCES tb_transportadora (id)
 );
 
--- Tabela de Promoções
+-- Tabela para armazenar Promoções
 CREATE TABLE IF NOT EXISTS tb_promocao
 (
     id          BIGINT UNSIGNED AUTO_INCREMENT,
@@ -366,20 +367,19 @@ CREATE TABLE IF NOT EXISTS tb_promocao
     PRIMARY KEY (id)
 );
 
--- Tabela de associação N:N entre Promoções e Produtos
+-- Tabela de Associação N:N entre Promoções e Produtos
 CREATE TABLE IF NOT EXISTS tb_promocao_produto
 (
     id_produto  BIGINT UNSIGNED NOT NULL,
     id_promocao BIGINT UNSIGNED NOT NULL,
-    desconto    DECIMAL(10, 2)  NOT NULL,  -- Desconto da promoção
+    desconto    DECIMAL(10, 2)  NOT NULL,  -- Desconto aplicado na promoção
 
-    PRIMARY KEY (id_produto, id_promocao), -- Garante que um produto participe de uma promoção apenas uma vez
-
+    PRIMARY KEY (id_produto, id_promocao), -- Garante unicidade na associação
     FOREIGN KEY (id_produto) REFERENCES tb_produto (id),
     FOREIGN KEY (id_promocao) REFERENCES tb_promocao (id)
 );
 
--- Tabela de Cupons
+-- Tabela para armazenar Cupons de Desconto
 CREATE TABLE IF NOT EXISTS tb_cupom
 (
     id          BIGINT UNSIGNED AUTO_INCREMENT,
@@ -391,7 +391,7 @@ CREATE TABLE IF NOT EXISTS tb_cupom
     PRIMARY KEY (id)
 );
 
--- Tabela de associação N:N entre Cupons e Compras
+-- Tabela de Associação N:N entre Cupons e Compras
 CREATE TABLE IF NOT EXISTS tb_cupom_compra
 (
     id_compra BIGINT UNSIGNED NOT NULL,
