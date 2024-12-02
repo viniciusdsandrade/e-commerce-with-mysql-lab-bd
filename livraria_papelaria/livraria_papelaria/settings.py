a76cd80ce36e6541b8d9c7c7620d69a5fb2af147
Felipe <felipe.arnemom@gmail.com>
2024-12-02 19:20:19 -0300

    Setup para o MySQL em comentários

--- livraria_papelaria/livraria_papelaria/settings.py
+++ livraria_papelaria/livraria_papelaria/settings.py
@@ -83,6 +83,19 @@ DATABASES = {
     }
 }
 
+# É preciso instalar a biblioteca pymysql pro MySQL funcionar, mas ele não foi
+# colocado no requirements.txt por enquanto porque estamos testando com SQLite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'livraria_papelaria',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
